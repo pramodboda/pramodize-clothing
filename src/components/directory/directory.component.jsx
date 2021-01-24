2fcba8f45d0a01f239cbc554b77a5b8e95d0248a
Pramod Boda <{ID}+{username}@users.noreply.github.com>
2021-01-24 05:51:03 +0530

    Broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.sass";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl:
+            "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
+          id: 1,
+        },
+        {
+          title: "jackets",
+          imageUrl:
+            "https://images.unsplash.com/photo-1611121539713-11617fcf6876?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+          id: 2,
+        },
+        {
+          title: "sneakers",
+          imageUrl:
+            "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+          id: 3,
+        },
+        {
+          title: "mens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1561926811-f64405c74398?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1026&q=80",
+          size: "large",
+          id: 4,
+        },
+        {
+          title: "womens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1503342452485-86b7f54527ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+          size: "large",
+          id: 5,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
---
+++ src/pages/homepage/homepage-component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './homepage.styles.sass';
+import Directory from '../../components/directory/directory.component';
+
+
+
+
+
+const HomePage  = () => (
+    <div className="homepage">
+        <Directory/>
+    </div>
+);
+
+export default HomePage;
