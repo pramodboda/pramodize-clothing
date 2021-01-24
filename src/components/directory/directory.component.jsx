080ecb50c9f3771806aeebddebded7e7e04dad3a
Pramod Boda <{ID}+{username}@users.noreply.github.com>
2021-01-24 21:04:01 +0530

    update our menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,18 +14,21 @@ class Directory extends React.Component {
           imageUrl:
             "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
           id: 1,
+          linkUrl:'hats'
         },
         {
           title: "jackets",
           imageUrl:
             "https://images.unsplash.com/photo-1611121539713-11617fcf6876?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
           id: 2,
+          linkUrl:'jackets'
         },
         {
           title: "sneakers",
           imageUrl:
             "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
           id: 3,
+          linkUrl:'sneakers'
         },
         {
           title: "mens",
@@ -33,6 +36,7 @@ class Directory extends React.Component {
             "https://images.unsplash.com/photo-1561926811-f64405c74398?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1026&q=80",
           size: "large",
           id: 4,
+          linkUrl:'mens'
         },
         {
           title: "womens",
@@ -40,6 +44,7 @@ class Directory extends React.Component {
             "https://images.unsplash.com/photo-1503342452485-86b7f54527ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
           size: "large",
           id: 5,
+          linkUrl:'womens'
         },
       ],
     };
@@ -48,8 +53,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'; 
 
 import './menu-item.styles.sass';
 
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`menu-item ${size}`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}></div>
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
@@ -14,4 +15,4 @@ const MenuItem = ({title, imageUrl, size}) => (
 )
 
 
-export default MenuItem;
+export default withRouter(MenuItem);
